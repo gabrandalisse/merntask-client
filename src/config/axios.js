a0f0e4cfd0322b83500bb36b50731fddec40b2bb
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2021-03-07 15:46:20 -0300

    changed baseurl

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: 'https://merntasks-server-gab.herokuapp.com/'
 });
 
 export default clienteAxios;
